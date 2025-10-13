d014e059b37c34d0aca859aa5942af733ce7a958
Night-Owl-Knight <117783808+Night-Owl-Knight@users.noreply.github.com>
2025-10-13 17:53:18 -0500

    Remove redundant rollset include

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,12 @@
 
 
 #include "headers/dice.h"
-#include "headers/rollset.h"
 #include "headers/favorite.h"
 #include "headers/menu.h"
 
 int main() {
 
     gameContext ctx(4);
-
-    //bool running = true;
-
-    //int userSide = 2;
-
-
-
 
     while (isRunning()) {
         showMenu("data/main-menu.txt");
